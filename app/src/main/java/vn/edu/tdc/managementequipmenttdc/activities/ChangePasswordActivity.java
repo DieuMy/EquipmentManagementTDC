272f9be17c9e5cd0175b7b7c1e182dccaae3da16
Nguyen Thi Thanh Huong <thanhhuongit94@gmail.com>
2019-04-16 13:36:29 +0700

    Xử lý hoàn tất màn hình đổi mật khẩu.(100%)

--- app/src/main/java/vn/edu/tdc/managementequipmenttdc/activities/ChangePasswordActivity.java
+++ app/src/main/java/vn/edu/tdc/managementequipmenttdc/activities/ChangePasswordActivity.java
@@ -9,16 +9,23 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import vn.edu.tdc.managementequipmenttdc.R;
 
-public class ChangePasswordActivity  extends AppCompatActivity {
+public class ChangePasswordActivity extends AppCompatActivity {
     private ImageView imgToolBarBack;
     private Button btnCancel;
     private Button btnSave;
@@ -29,6 +36,8 @@ public class ChangePasswordActivity  extends AppCompatActivity {
     FirebaseAuth firebaseAuth;
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
+
+    String currentPassword = "", newPassword = "", cofirmPassword = "";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -66,15 +75,9 @@ public class ChangePasswordActivity  extends AppCompatActivity {
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String currentPassword = edtCurrentPassword.getText().toString();
-                String newPassword = edtNewPassword.getText().toString();
-                String cofirmPassword = edtCofirmPassword.getText().toString();
-
-                //Kiem tra password hien tai co dung khong
-
-
-                //Kiem tra pass moi co > 6 ky tu va khong co khoang trang khong
-
+                currentPassword = edtCurrentPassword.getText().toString();
+                newPassword = edtNewPassword.getText().toString();
+                cofirmPassword = edtCofirmPassword.getText().toString();
 
                 //Kiem tra pass moi va pass hien tai co giong nhau khong?
                 if (newPassword.equals(currentPassword)) {
@@ -95,12 +98,7 @@ public class ChangePasswordActivity  extends AppCompatActivity {
                 builder.setPositiveButton("Đồng ý", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                       // changePassword();
-                        Toast.makeText(ChangePasswordActivity.this, "Đổi mật khẩu thành công", Toast.LENGTH_SHORT).show();
-                        firebaseAuth.signOut();
-                        Intent intent = new Intent(ChangePasswordActivity.this, LoginActivity.class);
-                        startActivity(intent);
-                        finishAffinity();
+                        changePassword();
                     }
                 });
 
@@ -117,7 +115,51 @@ public class ChangePasswordActivity  extends AppCompatActivity {
 
     }
 
-    private void changePassword(){
+    private void changePassword() {
+        final FirebaseUser user = firebaseAuth.getCurrentUser();
+        String account = user.getEmail();
+        AuthCredential authCredential = EmailAuthProvider.getCredential(account, currentPassword);
+        user.reauthenticate(authCredential).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    user.updatePassword(newPassword).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (!task.isSuccessful()) {
+                                View view = findViewById(R.id.changePass_layout);
+                                final Snackbar snackbar_fail = Snackbar.make(view, "Mật khẩu mới quá yếu. Vui lòng thử lại từ 6 ký tự trở lên!", Snackbar.LENGTH_LONG);
+                                snackbar_fail.show();
+                                snackbar_fail.setAction("Ẩn", new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        snackbar_fail.dismiss();
+                                    }
+                                });
+                            } else {
+                                View view = findViewById(R.id.changePass_layout);
+                                final Snackbar snackbar_success = Snackbar.make(view, "Thay đổi mật khẩu thành công! Vui lòng đăng nhập lại", Snackbar.LENGTH_LONG);
+                                snackbar_success.show();
+                                firebaseAuth.signOut();
+                                Intent intent = new Intent(ChangePasswordActivity.this, LoginActivity.class);
+                                startActivity(intent);
+                                finishAffinity();
 
+                            }
+                        }
+                    });
+                } else {
+                    View view = findViewById(R.id.changePass_layout);
+                    final Snackbar snackbar = Snackbar.make(view, "Mật khẩu hiện tại không chính xác!", Snackbar.LENGTH_SHORT);
+                    snackbar.show();
+                    snackbar.setAction("Ẩn", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            snackbar.dismiss();
+                        }
+                    });
+                }
+            }
+        });
     }
 }
